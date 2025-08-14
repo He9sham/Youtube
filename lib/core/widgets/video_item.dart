30a93ad7d691417f57e89698a7f75ce966591b99
DESKTOP-B5HI9JS\hp <heshamhamdan51@gmail.com>
2025-08-14 13:09:41 +0300

    Added HTTP dependency, internet permission in AndroidManifest, and implemented video list in HomePage with VideoItem widget.

---
+++ lib/core/widgets/video_item.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_app/core/widgets/custom_text.dart';
+
+class VideoItem extends StatelessWidget {
+  const VideoItem({
+    super.key,
+    required this.thumbnail,
+    required this.title,
+    required this.channelName,
+    required this.timing,
+    required this.views,
+  });
+  final String thumbnail, title, channelName, timing, views;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Image.network(
+              thumbnail,
+              height: 350,
+              errorBuilder: (context, error, stackTrace) {
+                return Container(
+                  color: Colors.white,
+                  alignment: Alignment.center,
+                  child: const Icon(
+                    Icons.broken_image,
+                    size: 40,
+                    color: Colors.black54,
+                  ),
+                );
+              },
+            ),
+            Positioned(
+              bottom: 5,
+              right: 10,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                color: Colors.black54,
+                child: CustomText(
+                  title: '99 : 08',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ],
+        ),
+
+        SizedBox(height: 15),
+
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Row(
+            children: [
+              CircleAvatar(radius: 20),
+
+              SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomText(title: title, fontWeight: FontWeight.bold),
+                  CustomText(
+                    title: '$channelName    $views views   $timing',
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_app/core/widgets/appbar_widget.dart';
+import 'package:youtube_app/core/widgets/video_item.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -35,6 +36,23 @@ class _HomePageState extends State<HomePage> {
             onSubmitted: (value) {},
             onTap: () {},
           ),
+        ),
+
+        body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: VideoItem(
+                thumbnail:
+                    'https://plus.unsplash.com/premium_photo-1664392147011-2a720f214e01?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D',
+                channelName: 'Hesham',
+                timing: '8 day ago',
+                views: '490k',
+                title: 'Movie',
+              ),
+            );
+          },
         ),
       ),
     );
